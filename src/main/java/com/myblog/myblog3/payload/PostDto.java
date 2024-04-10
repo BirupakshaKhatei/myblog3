b3b85f96cbf409e6db6b89d684aacde857bef1e4
biruk <birumd2625@gmail.com>
2024-04-10 22:27:31 +0530

    127:Create save Post Feature Entity class added 4th jan 2024

--- src/main/java/com/myblog/myblog3/payload/PostDto.java
+++ src/main/java/com/myblog/myblog3/payload/PostDto.java
@@ -1,20 +1,15 @@
-package com.myblog.myblog3.entity;
+package com.myblog.myblog3.payload;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
-@Entity
-@Table(name ="datas")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Post {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class PostDto {
     private long id;
+
     private String title;
     private String description;
     private String content;
---
+++ src/main/java/com/myblog/myblog3/service/PostService.java
@@ -0,0 +1,11 @@
+package com.myblog.myblog3.service;
+
+import com.myblog.myblog3.payload.PostDto;
+
+public interface PostService {
+    PostDto createPost(PostDto postDto);
+
+
+
+
+}
